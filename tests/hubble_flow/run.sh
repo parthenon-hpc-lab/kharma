46ce4d6ddf8068c386ed0c6413774a5d0d41f9e3
Vedant Dhruv <vdhruv2@illinois.edu>
2026-08-28 09:48:20 -0500

    hubble flow seems to work - converges at second order. not strang splitting as of now

--- tests/hubble_flow/run.sh
+++ tests/hubble_flow/run.sh
@@ -2,34 +2,36 @@
 set -euo pipefail
 
 # Bash script to run Hubble flow e- heating test
+# Ressler+ 2015 sec. 4.1.1: the explicit heating term must be time-centred, so
+# everything here should converge at 2nd order.
 
 # Set paths
 KHARMADIR=../..
 
 exit_code=0
 
-noh_test() {
-    ALL_RES="512"
-    for res in 512
+hubble_test() {
+    ALL_RES="64,128,256,512"
+    for res in 64 128 256 512
     do
-        eighth=$(($res / 8))
-        $KHARMADIR/run.sh -d . -i $KHARMADIR/pars/electrons/hubble.par debug/verbose=1 parthenon/output0/dt=1000 \
-                            parthenon/mesh/nx1=$res parthenon/meshblock/nx1=$eighth \
+        $KHARMADIR/run.sh -d . -i $KHARMADIR/pars/electrons/hubble.par debug/verbose=1 \
+                            parthenon/mesh/nx1=$res parthenon/meshblock/nx1=$res \
+                            parthenon/output0/dt=1e5 \
                             >log_hubble_${res}.txt 2>&1
 
-        #cp hubble.out0.final.phdf hubble.out0.final.res$res.phdf
+        mv hubble.out0.final.phdf hubble.out0.final.res$res.phdf
     done
+
     check_code=0
-    python make_plots.py
-    #python check.py . . $ALL_RES 1.666667 || check_code=$?
-    #if [[ $check_code != 0 ]]; then
-    #    echo Noh shock test FAIL: $check_code
-    #    exit_code=1
-    #else
-    #    echo Noh shock test success
-    #fi
+    python3 check.py ${ALL_RES//,/ } || check_code=$?
+    if [[ $check_code != 0 ]]; then
+        echo Hubble flow test FAIL: $check_code
+        exit_code=1
+    else
+        echo Hubble flow test success
+    fi
 }
 
-noh_test
+hubble_test
 
 exit $exit_code
