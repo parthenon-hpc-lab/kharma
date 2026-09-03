2eb270b0b02517f1f6fa8e992a05d65bf87f2c1b
Vedant Dhruv <vdhruv2@illinois.edu>
2026-09-03 09:20:44 -0500

    fixes in types.hpp

--- kharma/types.hpp
+++ kharma/types.hpp
@@ -238,15 +238,14 @@ class VarMap
             T3ADD = -1;
         }
     }
-}
 
     void print() const
-{
-    printf("VAR MAP:\n");
-    printf("prims: %d %d %d %d %d\n", RHO, UU, U1, U2, U3);
-    printf("B field cell: %d %d %d face: %d %d %d\n", B1, B2, B3, Bf1, Bf2, Bf3);
-    printf("EMHD q: %d dP: %d\n", Q, DP);
-}
+    {
+        printf("VAR MAP:\n");
+        printf("prims: %d %d %d %d %d\n", RHO, UU, U1, U2, U3);
+        printf("B field cell: %d %d %d face: %d %d %d\n", B1, B2, B3, Bf1, Bf2, Bf3);
+        printf("EMHD q: %d dP: %d\n", Q, DP);
+    }
 }
 ;
 
